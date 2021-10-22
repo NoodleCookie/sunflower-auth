f24a586f9ad1f4f1d5c11304cf9695d1a8fea1c7
Izumi <1042631369@qq.com>
2021-10-22 15:50:44 +0800

    ref

--- src/main/java/sunflower/entity/SunUser.java
+++ src/main/java/sunflower/entity/SunUser.java
@@ -1,5 +1,6 @@
 package sunflower.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
@@ -21,19 +22,21 @@ public class SunUser {
 
     @Id
     @Column(name = "sun_id")
+    @JsonIgnore
     private String id;
 
     @Column(name = "sun_username")
     private String username;
 
     @Column(name = "sun_password")
+    @JsonIgnore
     private String password;
 
     private String nickName;
 
     @CreatedDate
     @Column(name = "sun_create_time")
-    @JsonIgnore
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone="GMT+8")
     private Date createdTime;
 
     @Column(name = "sun_enable")
