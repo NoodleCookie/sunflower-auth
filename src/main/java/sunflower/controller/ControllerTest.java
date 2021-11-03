1a1254e876998b982977a84b16f2a03c46b4cd8a
Izumi <1042631369@qq.com>
2021-11-03 11:28:48 +0800

    delete test value in proflie

--- src/main/java/sunflower/controller/ControllerTest.java
+++ src/main/java/sunflower/controller/ControllerTest.java
@@ -11,11 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 public class ControllerTest {
 
-    @Value("${test}")
-    private String test;
 
     @GetMapping
     public String test() {
-        return "welcome to sunflower auth server: " + test;
+        return "welcome to sunflower auth server";
     }
 }
